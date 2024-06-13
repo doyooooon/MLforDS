e2d84357bef32a8b175fa3a5ecbb1cd71f43b9b5
Doyun Kwon <161291926+doyooooon@users.noreply.github.com>
2024-06-13 17:03:15 +0900

    Add files via upload

---
+++ download_data.sh
@@ -0,0 +1,5 @@
+# Google Drive folder ID
+FOLDER_ID="1fyfeqMWtOeF1N32g4_R0Q4JpHpKQc7yg"
+
+# Download the Google Drive folder content to the current directory
+gdown --folder https://drive.google.com/drive/folders/$FOLDER_ID --remaining-ok
